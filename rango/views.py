9ba73b8ec97998a3221e79d6b80e18845347deca
Ruchira Ravishankar <2300294r@student.gla.ac.uk>
2019-01-21 19:35:10 +0000

    until ch 4

--- rango/views.py
+++ rango/views.py
@@ -9,4 +9,5 @@ def index(request):
     #return HttpResponse("Rango says hey there partner! <br/><a href='/rango/about/'>About</a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about page<br/><a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
+    #return HttpResponse("Rango says here is the about page<br/><a href='/rango/'>Index</a>")
