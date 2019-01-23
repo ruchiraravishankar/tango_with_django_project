65163ea52f7e6a3ce9b1444b3c4f57423e521c07
Ruchira Ravishankar <2300294r@student.gla.ac.uk>
2019-01-23 22:37:31 +0000

    ch 5 complete

--- populate_rango.py
+++ populate_rango.py
@@ -27,11 +27,11 @@ def populate():
          "url":"http://bottlepy.org/docs/dev/"},
         {"title":"Flask",
          "url":"http://flask.pocoo.org"} ]
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages} }
+    cats = {"Python": {"pages": python_pages,"views":128,"likes":64},
+            "Django": {"pages": django_pages,"views":64,"likes":32},
+            "Other Frameworks": {"pages": other_pages,"views":32,"likes":16} }
     for cat, cat_data in cats.items():
-        c= add_cat(cat)
+        c= add_cat(cat,cat_data[1],cat_data[2])
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
     # Print out the categories we have added.
@@ -44,8 +44,8 @@ def add_page(cat, title, url, views=0):
     p.views=views
     p.save()
     return p
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name,views=views,likes=likes)[0]
     c.save()
     return c
 # Start execution here!
--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from rango.models import Category, Page
-admin.site.register(Category)
-admin.site.register(Page)
+
 
 # Register your models here.
+admin.site.register(Category)
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+admin.site.register(Page,PageAdmin)
