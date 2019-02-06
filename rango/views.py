cee88b79347977aaa75a4f6a4c0a8503c316de21
Ruchira Ravishankar <2300294r@student.gla.ac.uk>
2019-02-06 01:36:08 +0000

    end of ch 8

--- rango/views.py
+++ rango/views.py
@@ -18,32 +18,26 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'rango/about.html')
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html', {})
+
+    # return render(request, 'rango/about.html')
     # return HttpResponse("Rango says here is the about page<br/><a href='/rango/'>Index</a>")
 
 
 def show_category(request, category_name_slug):
     context_dict = {}
     try:
-        # Can we find a category name slug with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # So the .get() method returns one model instance or raises an exception.
+
         category = Category.objects.get(slug=category_name_slug)
-        # Retrieve all of the associated pages.
-        # Note that filter() will return a list of page objects or an empty list
         pages = Page.objects.filter(category=category)
-        # Adds our results list to the template context under name pages.
         context_dict['pages'] = pages
-        # We also add the category object from
-        # the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
         context_dict['category'] = category
     except Category.DoesNotExist:
-        # We get here if we didn't find the specified category. # Don't do anything -
-        # the template will display the "no category" message for us.
+
         context_dict['category'] = None
         context_dict['pages'] = None
-    # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context_dict)
 
 
